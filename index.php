23ba52f03765ad72a71dc77609db19299f64835a
Jos Bleeker <j.j.bleeker@horizoncollege.nl>
2019-09-30 16:07:38 +0200

    title and content added

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Project1</title>
+  </head>
+  <body>
+    <h1>Project 1</h1>
+    <?php
+      echo "<p>Dit is index.php van project 1</p>"
+     ?>
+  </body>
+</html>
